ed9a29c90ff6bf59cf929cda02322e1f304798c0
sh0kh <shoxruuh07tuxtanazarov@gmail.com>
2024-12-03 17:00:18 +0500

    edit

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -60,7 +60,7 @@ const Sidebar = () => {
                     <div role="button"
                         class={`flex items-center gap-[20px] w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900 ${Admins ? "bg-blue-gray-50 bg-opacity-80 text-blue-gray-900" : ''}`}>
                         <IoPeopleSharp fontSize={30} />
-                        Xodimlar
+                        Ustozlar
                     </div>
                 </NavLink>
                 <NavLink to={'/payments'}>
--- src/Pages/Admins.jsx
+++ src/Pages/Admins.jsx
@@ -13,7 +13,7 @@ export default function Admins() {
         <div className="Admins p-[15px]">
             <div className="Header__wrapper">
                 <h1 className="text-MainColor text-[32px] font-[700]">
-                    Xodimlar
+                    Ustozlar
                 </h1>
                 <button onClick={() => setCreateModal(true)} className="bg-MainColor text-[white] rounded-[10px] p-[10px] border-[2px] border-MainColor duration-500 px-[20px] hover:text-MainColor hover:bg-[white]">
                     Yaratish
